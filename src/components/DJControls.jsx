4503513d33e2f51781742e3169f78241a814be44
Seb17238 <farsb001@mymail.unisa.edu.au>
2025-11-07 01:53:22 +1030

    Added a volume slider and check boxes to the djcontrols

--- src/components/DJControls.jsx
+++ src/components/DJControls.jsx
@@ -2,7 +2,36 @@ function DJControls(){
     return (
         <>
 
-                    <div className="col-md-4">
+
+            <div class="input-group mb-3">
+                <span className="input-group-text" id="cpm_label">setCPM</span>
+                <input type="text" className="form-control" id="cpm_text_input" placeholder="120" aria-label="Username" aria-describedby="cpm_label"/>
+
+                </div>
+                <div>
+                    <label htmlFor="volume_range" className="form-label">volume</label>
+                    <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range"/>
+                </div>
+
+                <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="s1"/>
+                <label className="form-check-label" htmlFor="s1">
+                    s1
+                </label>
+                </div>
+                <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="s2"/>
+                <label className="form-check-label" htmlFor="s2">
+                    s2
+                </label>
+                </div>
+                <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="s3"/>
+                <label className="form-check-label" htmlFor="s3">
+                    s3
+                </label>
+                </div>
+                    {/* <div className="col-md-4">
                         <div className="form-check">
                             <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
                             <label className="form-check-label" htmlFor="flexRadioDefault1">
@@ -15,7 +44,7 @@ function DJControls(){
                                 p1: HUSH
                             </label>
                         </div>
-                    </div>
+                    </div> */}
 
 
         </>
